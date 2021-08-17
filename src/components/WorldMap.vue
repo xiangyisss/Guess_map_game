de683ecc792ac4da9604e6aca95d288a00473c68
xiangyisss <xiangyisunn@gmail.com>
2021-08-17 14:39:11 +0200

    when failed show correct country is done

--- src/components/WorldMap.vue
+++ src/components/WorldMap.vue
@@ -1,9 +1,21 @@
 <template>
 	
-	<div>
-	<h4>Find this country <span>{{randomCountry.countryName}} </span> </h4>
+	<div class="container">
+	<div class="gameover" v-if="gameOver"><h1>Game over</h1></div>
+	<div class="info_bar">
+
+	<div class="countryname_flag">
+	<h4>Find this country <span>{{randomCountry.countryName}}</span> </h4>
+	<img :src="`https://www.countryflags.io/${randomCountry.countryId}/shiny/32.png`">
+	</div>
+	
+	<div class="score_life">
+	<h4 class="score">Score : {{score}}</h4>
+	<h4>Lifes : {{lifes}}</h4>
+	</div>
+	</div>
 	<!-- <button @click="startGame">Start</button> -->
-	<div id="chartdiv"></div>
+	<div id="chartdiv" ></div>
 	</div>
 	
   
@@ -14,13 +26,13 @@ import { defineComponent, ref, Ref} from "vue";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
-import { any } from "@amcharts/amcharts4/.internal/core/utils/Array";
+// import { any } from "@amcharts/amcharts4/.internal/core/utils/Array";
 
 interface country {
 	countryId : string,
 	countryName : string
 }
-// import Map from "./Map.vue";
+
 
 export default defineComponent({
   name: "WorldMap",
@@ -40,15 +52,14 @@ export default defineComponent({
 
 	let mapTemplate = WorldMap.mapPolygons.template;
 	// mapTemplate.tooltipText = "{name}";
-	// mapTemplate.tooltipText = "{id}";
 
-	mapTemplate.fill = am4core.color("#cce1e9");
+	//testing
+
+	//testing --end
+	// mapTemplate.fill = am4core.color("#98EA8A");
 
 	let hs = mapTemplate.states.create("hover");
-	hs.properties.fill = am4core.color("#2a6f97");
-
-
-
+	hs.properties.fill = am4core.color("#6ED35D");
 
 	let countryList : Ref<any[]> = ref([])
 	let countryNamdAndId : Ref<any[]> = ref([])
@@ -56,8 +67,14 @@ export default defineComponent({
 		countryId : '',
 		countryName : ''
 	})
+	let score : Ref<number> = ref(0)
+	let lifes : Ref<number> = ref(3)
+	let gameOver : Ref<boolean> = ref(false)
+	
+	
 
 	countryList.value  = WorldMap.data
+	// console.log(countryList)
 
 	const getRandomCountry = () => {
 		let randomNumber = Math.floor(Math.random() * countryNamdAndId.value.length)
@@ -77,34 +94,46 @@ export default defineComponent({
 			
 			countryNamdAndId.value.push(countryInfo)
 		}	
-		console.log('i am running')
 		getRandomCountry()
 	}, 0)
 	
-	
-
-	
-	
-	// setTimeout(() => {
-
-	// 	for (let item of countryList.value) {
-	// 		let countryInfo = {
-	// 			countryName: item.name,
-	// 			countryId: item.id
-	// 		}
-			
-	// 		countryNamdAndId.value.push(countryInfo)
-	// 	}		
+	function myFunction(ev : any) {
 		
-	// 	getRandomCountry()
+		// console.log(ev.target.dataItem.dataContext.name);
+		if(randomCountry.value.countryName === ev.target.dataItem.dataContext.name) {
+			score.value +=1;
+			ev.target.fill =am4core.color("#F3CC3B")
+			getRandomCountry()
+			return score
+		} else {
+			
+			lifes.value -= 1
+			ev.target.fill =am4core.color("#FF0000")
+		}
+		if(lifes.value === 0) {
+			let answer = WorldMap.getPolygonById(randomCountry.value.countryId);
+			answer.isHover = true;
+			mapTemplate.tooltipText = "{randomCountry.value.countryName}";
+			gameOver.value = true
+		}
+		// console.log('You are wrong')
 
-	// })
+	}
+	
+	mapTemplate.events.on("hit", myFunction);
+	
+	//map.series.template.events.off("hit", myFunction, this);
+		
+	
 	
 	
 	return {
 		randomCountry,
 		countryList,
 		countryNamdAndId,
+		score,
+		lifes,
+		gameOver
 	}
 }    
 });
@@ -112,8 +141,22 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.container{
+	overflow: hidden;
+}
 h1 {
-  color: white;
+  color: blue;
+}
+
+.info_bar, .countryname_flag, .score_life {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+}
+
+span, .score {
+	margin-right: 1rem;
 }
 .map_area {
   width: 80%;
@@ -127,7 +170,7 @@ h1 {
   height: 500px;
 }
 
-svg path {
+/* svg path {
   fill: #264653;
   stroke: whitesmoke;
   stroke-width: 0.25;
@@ -142,13 +185,10 @@ svg path:hover {
 svg {
   width: 100%;
   height: 100%;
-}
+} */
 
 span {
 	color: brown;
 }
-/* svg {
-   width="1009.6727"
-   height="665.96301"
-} */
+
 </style>
